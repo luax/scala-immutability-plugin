75de1dc2e37ba474aadcc38d9b3f3962a30aaaca
Ludvig Axelsson <ludvigaxelsson@gmail.com>
2016-11-12 15:18:37 +0100

    Hello world

---
+++ build.sbt
@@ -0,0 +1,12 @@
+import Dependencies._
+
+lazy val commonSettings = Seq(
+  version := "1.0.0",
+  scalaVersion := "2.11.8"
+)
+
+lazy val root = (project in file(".")).
+  settings(commonSettings: _*).
+  settings(
+    libraryDependencies ++= backendDeps
+  )
---
+++ project/Dependencies.scala
@@ -0,0 +1,14 @@
+import sbt._
+
+object Dependencies {
+  // Versions
+  lazy val scalatestVersion = "3.0.0"
+
+  // Libraries
+  val scalatic = "org.scalactic" %% "scalactic" % scalatestVersion
+  val scalatest = "org.scalatest" %% "scalatest" % scalatestVersion
+
+  // Projects
+  val backendDeps =
+    Seq(scalatic, scalatest % Test)
+}
